6aa2dfbbb7f9699fb0e7e2c01cd22c69a8ead846
azptmdr <ksgarciap@unbosque.edu.co>
2022-04-18 09:18:24 -0500

    Capacitancia

---
+++ src/co/edu/unbosque/view/Panel2.java
@@ -0,0 +1,106 @@
+package co.edu.unbosque.view;
+
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+import java.awt.Font;
+
+import javax.swing.JButton;
+import javax.swing.JCheckBox;
+import javax.swing.JLabel;
+import javax.swing.JRadioButton;
+
+public class Panel2 extends JPanel {
+	private JTextField textField_2;
+
+	/**
+	 * Create the panel.
+	 */
+	public Panel2() {
+		JPanel panel = new JPanel();
+		panel.setBounds(10, 121, 504, 268);
+		panel.setLayout(null);
+		
+		JLabel lblNewLabel_1 = new JLabel("Epsilon");
+		lblNewLabel_1.setBounds(142, 11, 35, 14);
+		panel.add(lblNewLabel_1);
+		
+		JLabel lblNewLabel_3 = new JLabel("Distancia");
+		lblNewLabel_3.setFont(new Font("Cambria Math", Font.PLAIN, 15));
+		lblNewLabel_3.setBounds(50, 88, 61, 14);
+		panel.add(lblNewLabel_3);
+		
+		JTextField textFieldCarga = new JTextField();
+		textFieldCarga.setBounds(121, 84, 86, 20);
+		panel.add(textFieldCarga);
+		textFieldCarga.setColumns(10);
+		
+		JLabel lblNewLabel_3_1 = new JLabel("m");
+		lblNewLabel_3_1.setFont(new Font("Cambria Math", Font.PLAIN, 15));
+		lblNewLabel_3_1.setBounds(217, 88, 16, 14);
+		panel.add(lblNewLabel_3_1);
+		
+		JLabel lblNewLabel_3_3 = new JLabel("Area");
+		lblNewLabel_3_3.setFont(new Font("Cambria Math", Font.PLAIN, 15));
+		lblNewLabel_3_3.setBounds(60, 124, 44, 14);
+		panel.add(lblNewLabel_3_3);
+		
+		JTextField textField = new JTextField();
+		textField.setColumns(10);
+		textField.setBounds(121, 120, 86, 20);
+		panel.add(textField);
+		
+		JLabel lblNewLabel_3_1_1 = new JLabel("m\u00B2");
+		lblNewLabel_3_1_1.setFont(new Font("Cambria Math", Font.PLAIN, 15));
+		lblNewLabel_3_1_1.setBounds(217, 119, 27, 25);
+		panel.add(lblNewLabel_3_1_1);
+		
+		JButton btnNewButton = new JButton("Resolver");
+		btnNewButton.setFont(new Font("Franklin Gothic Demi", Font.PLAIN, 15));
+		btnNewButton.setBounds(200, 165, 106, 39);
+		panel.add(btnNewButton);
+		
+		JLabel lblNewLabel_3_4 = new JLabel("Capacitancia =");
+		lblNewLabel_3_4.setFont(new Font("Cambria Math", Font.PLAIN, 15));
+		lblNewLabel_3_4.setBounds(50, 228, 93, 14);
+		panel.add(lblNewLabel_3_4);
+		
+		JLabel lblM_1 = new JLabel("N");
+		lblM_1.setBounds(416, 215, 16, 27);
+		panel.add(lblM_1);
+		lblM_1.setFont(new Font("Cambria Math", Font.PLAIN, 15));
+		
+		JLabel lblM_1_2 = new JLabel("_____");
+		lblM_1_2.setBounds(406, 230, 35, 10);
+		panel.add(lblM_1_2);
+		lblM_1_2.setFont(new Font("Cambria Math", Font.PLAIN, 15));
+		
+		JLabel lblM_1_1 = new JLabel("C");
+		lblM_1_1.setBounds(417, 240, 42, 28);
+		panel.add(lblM_1_1);
+		lblM_1_1.setFont(new Font("Cambria Math", Font.PLAIN, 15));
+		
+		JTextField textField_1 = new JTextField();
+		textField_1.setBounds(153, 215, 251, 42);
+		panel.add(textField_1);
+		
+		JRadioButton rdbtnNewRadioButton = new JRadioButton("Default");
+		rdbtnNewRadioButton.setBounds(197, 7, 61, 23);
+		panel.add(rdbtnNewRadioButton);
+		
+		JRadioButton rdbtnNewRadioButton_1 = new JRadioButton("Otros");
+		rdbtnNewRadioButton_1.setBounds(281, 7, 53, 23);
+		panel.add(rdbtnNewRadioButton_1);
+		
+		JLabel lblNewLabel_3_2 = new JLabel("Epsilon");
+		lblNewLabel_3_2.setFont(new Font("Cambria Math", Font.PLAIN, 15));
+		lblNewLabel_3_2.setBounds(50, 49, 61, 14);
+		panel.add(lblNewLabel_3_2);
+		
+		textField_2 = new JTextField();
+		textField_2.setColumns(10);
+		textField_2.setBounds(121, 45, 86, 20);
+		panel.add(textField_2);
+
+	}
+}
---
+++ src/co/edu/unbosque/view/Paneles.java
@@ -0,0 +1,85 @@
+package co.edu.unbosque.view;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JButton;
+import java.awt.Font;
+import javax.swing.JLabel;
+import javax.swing.JCheckBox;
+import javax.swing.JRadioButton;
+import javax.swing.JTextField;
+
+public class Paneles extends JPanel {
+
+	private JPanel contentPane;
+	private JTextField textFieldCarga;
+	private JTextField textFieldNC1;
+	private JTextField textField;
+	private JTextField textField_1;
+	private Panel2 Panel2;
+	private Panel1 Panel1;
+	
+
+	/**
+	 * Create the frame.
+	 */
+	public Paneles() {
+		
+		
+		setBounds(100, 100, 520, 400);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(null);
+		
+
+
+		
+		JButton btnRegresar = new JButton("Regresar");
+		btnRegresar.setFont(new Font("Franklin Gothic Demi Cond", Font.PLAIN, 11));
+		btnRegresar.setBounds(0, 0, 89, 23);
+		contentPane.add(btnRegresar);
+		
+		JLabel lblNewLabel = new JLabel("Capacitancia Placas");
+		lblNewLabel.setFont(new Font("Franklin Gothic Demi Cond", Font.PLAIN, 36));
+		lblNewLabel.setBounds(114, 0, 282, 30);
+		contentPane.add(lblNewLabel);
+		
+		JLabel lblNewLabel_2 = new JLabel("Seleccione seg\u00FAn sus datos");
+		lblNewLabel_2.setBounds(67, 58, 131, 14);
+		contentPane.add(lblNewLabel_2);
+		
+		JRadioButton rdbtnNewRadioButton = new JRadioButton("Opci\u00F3n 1");
+		rdbtnNewRadioButton.setBounds(247, 54, 67, 23);
+		contentPane.add(rdbtnNewRadioButton);
+		
+		JRadioButton rdbtnNewRadioButton_1 = new JRadioButton("Opci\u00F3n 2");
+		rdbtnNewRadioButton_1.setBounds(349, 53, 67, 23);
+		contentPane.add(rdbtnNewRadioButton_1);
+		
+		Panel1 = new Panel1();
+		Panel1.setBounds(50, 185, 116, -135);
+		contentPane.add(Panel1);
+		
+	}
+	
+	public void ponerPanel1() {
+		
+		remove(Panel1);
+		
+		
+		
+	}
+
+	public void ponerPanel2() {
+		
+		remove(Panel1);
+		Panel2 = new Panel2();
+		
+		
+	}
+	
+}
